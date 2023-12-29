abc665ee9ca9bc7ef42ad4568cfdd61ca00264df
ranjansmygit <ranjan.smy@gmail.com>
2023-12-29 15:58:14 +0530

    CodeWarsSolidity

--- CountbyX.sol
+++ CountbyX.sol
@@ -1,6 +1,8 @@
+/* Create a function with two arguments that will return an array of the first n multiples of x.
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+Return the results as an array or list ( depending on language ). */
 // SPDX-License-Identifier: BSD-2-Clause
 pragma solidity ^0.8.16;
-
 contract CountByX {
 
   function countBy(uint256 x, uint256 n) public pure returns (uint256[] memory) {
